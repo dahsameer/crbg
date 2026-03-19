9c9b25501f65a41fa16aeb0c48f26ab7606468df
Sameer Dahal <dahsameer@gmail.com>
2026-03-19 13:08:25 +0545

    changed site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dahsameer.github.io',
-	base: 'crbg',
+	site: 'https://corbig.com',
 	vite: {
 		plugins: [tailwindcss()],
 	},
